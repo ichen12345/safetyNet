e9e3f3e7f8ebf7fb29911cdfb769412472366742
Ivy Chen <chenivy86085@gmail.com>
2024-03-11 22:23:38 -0400

    added working person controller tests

--- src/test/java/com/openclassrooms/safetyNet/controller/PersonControllerTest.java
+++ src/test/java/com/openclassrooms/safetyNet/controller/PersonControllerTest.java
@@ -6,7 +6,6 @@ import com.openclassrooms.safetyNet.service.PersonService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,15 +13,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-@WebMvcTest(controllers = PersonController.class)
+@WebMvcTest(PersonController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 class PersonControllerTest {
@@ -53,13 +50,31 @@ class PersonControllerTest {
 
     @Test
     public void PersonController_CreatePerson_ReturnCreated() throws Exception {
-        given(personService.createPerson(ArgumentMatchers.any())).willAnswer(invocation -> invocation.getArgument(0));
+        when(personService.createPerson(person)).thenReturn(person);
 
-        ResultActions response = mockMvc.perform(post("/person")
+        mockMvc.perform(post("/person")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(person)));
+                .content(objectMapper.writeValueAsString(person)))
+                .andExpect(status().isOk());
+    }
 
-        response.andExpect(MockMvcResultMatchers.status().isCreated());
+    @Test
+    public void UpdatePerson_ReturnUpdatedPerson() throws Exception {
+        when(personService.updatePerson(person)).thenReturn(person);
+
+        mockMvc.perform(put("/person/{id}", person.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(person)))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void DeletePersonById_ReturnNothing() throws Exception {
+        doNothing().when(personService).deletePerson(person.getId());
+        mockMvc.perform(delete("/person/{id}", person.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(person.getId())))
+                        .andExpect(status().isOk());
+
     }
 
 }
